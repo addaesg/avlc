bcb8b7e0df6543663dbb5d1e753d89ff31dc42e7
Matheus Paixão <mpgs@cin.ufpe.br>
2024-06-03 22:30:29 -0300

    Wip

--- Project2/game/Curve.py
+++ Project2/game/Curve.py
@@ -45,17 +45,129 @@ class MonomialInterpolatedCurve:
         self.samples = []
         self.dated = True
 
+    def back_sub(self, U, b):
+        """x = back_sub(U, b) is the solution to U x = b
+        U must be an upper-triangular matrix
+        b must be a vector of the same leading dimension as U
+        """
+        n = U.shape[0]
+        x = np.zeros(n)
+        for i in range(n-1, -1, -1):
+            tmp = b[i]
+            for j in range(i+1, n):
+                tmp -= U[i,j] * x[j]
+            x[i] = tmp / U[i,i]
+        return x
+
+    def forward_sub(self, L, b):
+        """x = forward_sub(L, b) is the solution to L x = b
+        L must be a lower-triangular matrix
+        b must be a vector of the same leading dimension as L
+        """
+        n = L.shape[0]
+        x = np.zeros(n)
+        for i in range(n):
+            tmp = b[i]
+            for j in range(i-1):
+                tmp -= L[i,j] * x[j]
+            x[i] = tmp / L[i,i]
+        return x
+
+    def lu_solve(self, L, U, b):
+        """x = lu_solve(L, U, b) is the solution to L U x = b
+        L must be a lower-triangular matrix
+        U must be an upper-triangular matrix of the same size as L
+        b must be a vector of the same leading dimension as L
+        """
+        y = self.forward_sub(L, b)
+        x = self.back_sub(U, y)
+        return x
+
+    def lup_solve(self, L, U, P, b):
+        """x = lup_solve(L, U, P, b) is the solution to L U x = P b
+        L must be a lower-triangular matrix
+        U must be an upper-triangular matrix of the same shape as L
+        P must be a permutation matrix of the same shape as L
+        b must be a vector of the same leading dimension as L
+        """
+        z = np.dot(P, b)
+        x = self.lu_solve(L, U, z)
+        return x
+
+    def lup_decomp(self, A):
+        """(L, U, P) = lup_decomp(A) is the LUP decomposition P A = L U
+        A is any matrix
+        L will be a lower-triangular matrix with 1 on the diagonal, the same shape as A
+        U will be an upper-triangular matrix, the same shape as A
+        U will be a permutation matrix, the same shape as A
+        """
+        n = A.shape[0]
+        if n == 1:
+            L = np.array([[1]])
+            U = A.copy()
+            P = np.array([[1]])
+            return (L, U, P)
+        
+        # Handle empty matrix case
+        if A.size == 0:
+            print("raised")
+            raise ValueError("Matrix A is empty")
+
+        i = np.argmax(A[:,0])
+        A_bar = np.vstack([A[i,:], A[:i,:], A[(i+1):,:]])
+
+        A_bar11 = A_bar[0,0]
+        A_bar12 = A_bar[0,1:]
+        A_bar21 = A_bar[1:,0]
+        A_bar22 = A_bar[1:,1:]
+
+        S22 = A_bar22 - np.dot(A_bar21, A_bar12) / A_bar11
+
+        (L22, U22, P22) = self.lup_decomp(S22)
+
+        L11 = 1
+        U11 = A_bar11
+
+        L12 = np.zeros(n-1)
+        U12 = A_bar12.copy()
+
+        L21 = np.dot(P22, A_bar21) / A_bar11
+        U21 = np.zeros(n-1)
+
+        L = np.block([[L11, L12], [L21, L22]])
+        U = np.block([[U11, U12], [U21, U22]])
+        P = np.block([
+            [np.zeros((1, i-1)), 1,                  np.zeros((1, n-i))],
+            [P22[:,:(i-1)],      np.zeros((n-1, 1)), P22[:,i:]]
+        ])
+        return (L, U, P)
+
+    def linear_solve(self, A, b):
+        """x = linear_solve(A, b) is the solution to A x = b (computed with partial pivoting)
+        A is any matrix
+        b is a vector of the same leading dimension as A
+        x will be a vector of the same leading dimension as A
+        """
+        (L, U, P) = self.lup_decomp(A)
+        x = self.lup_solve(L, U, P, b)
+        return x
+
     ## Monomial Interpolation
     def calculate(self):
         numberOfPoints = len(self.controlPoints)
         exponents = np.arange(numberOfPoints)
-        matrix = np.empty((0, numberOfPoints))
-        for p in self.controlPoints:
-            repeatedX = np.repeat(p.x, numberOfPoints)
-            matrixRow = [np.power(repeatedX, exponents)]
-            matrix = np.append(matrix, matrixRow, axis=0)
-
+        matrix = np.zeros((numberOfPoints, numberOfPoints))
+        column_vector_Y = np.zeros(numberOfPoints)
+        for idx, p in enumerate(self.controlPoints):
+            matrix[idx, :] = np.power(p.x, exponents)
+            column_vector_Y[idx] = p.y
         print(matrix)
+        try:
+            x = self.linear_solve(matrix, column_vector_Y)
+            print(x)
+        except:
+            print("não deu")
+            
 
     def draw(self, screen):
         self.calculate()
