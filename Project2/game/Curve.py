a5ead9920818eafa9286c9b2cce183d36d27339b
Matheus Paixão <mpgs@cin.ufpe.br>
2024-06-04 22:24:28 -0300

    Fix: negative matrix size solved

--- Project2/game/Curve.py
+++ Project2/game/Curve.py
@@ -107,40 +107,48 @@ class MonomialInterpolatedCurve:
             U = A.copy()
             P = np.array([[1]])
             return (L, U, P)
-        
-        # Handle empty matrix case
+
         if A.size == 0:
-            print("raised")
             raise ValueError("Matrix A is empty")
 
-        i = np.argmax(A[:,0])
-        A_bar = np.vstack([A[i,:], A[:i,:], A[(i+1):,:]])
+        i = np.argmax(np.abs(A[:, 0]))
+        A_bar = np.vstack([A[i, :], A[:i, :], A[(i + 1):, :]])
 
-        A_bar11 = A_bar[0,0]
-        A_bar12 = A_bar[0,1:]
-        A_bar21 = A_bar[1:,0]
-        A_bar22 = A_bar[1:,1:]
+        A_bar11 = A_bar[0, 0]
+        A_bar12 = A_bar[0, 1:]
+        A_bar21 = A_bar[1:, 0]
+        A_bar22 = A_bar[1:, 1:]
 
-        S22 = A_bar22 - np.dot(A_bar21, A_bar12) / A_bar11
+        if A_bar22.size == 0:
+            S22 = A_bar22
+            L22, U22, P22 = np.array([[]]), np.array([[]]), np.array([[]])
+        else:
+            S22 = A_bar22 - np.dot(A_bar21[:, np.newaxis], A_bar12[np.newaxis, :]) / A_bar11
+            (L22, U22, P22) = self.lup_decomp(S22)
 
-        (L22, U22, P22) = self.lup_decomp(S22)
+        L11 = np.array([[1]])
+        U11 = np.array([[A_bar11]])
 
-        L11 = 1
-        U11 = A_bar11
+        L12 = np.zeros((1, n-1))
+        U12 = A_bar12.reshape(1, -1)
 
-        L12 = np.zeros(n-1)
-        U12 = A_bar12.copy()
-
-        L21 = np.dot(P22, A_bar21) / A_bar11
-        U21 = np.zeros(n-1)
+        if A_bar21.size > 0:
+            L21 = (np.dot(P22, A_bar21) / A_bar11).reshape(-1, 1)
+        else:
+            L21 = np.zeros((n-1, 1))
+        U21 = np.zeros((n-1, 1))
 
         L = np.block([[L11, L12], [L21, L22]])
         U = np.block([[U11, U12], [U21, U22]])
-        P = np.block([
-            [np.zeros((1, i-1)), 1,                  np.zeros((1, n-i))],
-            [P22[:,:(i-1)],      np.zeros((n-1, 1)), P22[:,i:]]
-        ])
+
+        P_top = np.hstack([np.zeros((1, i)), np.ones((1, 1)), np.zeros((1, n-i-1))])
+        P_bottom_left = P22[:, :(i)] if i > 0 else np.zeros((n-1, 0))
+        P_bottom_right = P22[:, i:] if i < n-1 else np.zeros((n-1, 0))
+        P_bottom = np.hstack([P_bottom_left, np.zeros((n-1, 1)), P_bottom_right])
+        P = np.vstack([P_top, P_bottom])
+
         return (L, U, P)
+
 
     def linear_solve(self, A, b):
         """x = linear_solve(A, b) is the solution to A x = b (computed with partial pivoting)
